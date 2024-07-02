acfe7dc30cdb1e4b4e1e60386be01dab007a4dc8
jesusMS-rgb <jesus.arian.moreno@gmail.com>
2024-07-02 00:17:54 -0700

    Actualizacion 1
    La actualizacion en casa

--- Banco de Capacitores/Funciones.js
+++ Banco de Capacitores/Funciones.js
@@ -1,7 +1,9 @@
 const images = ['Imagenes/Imagen2.jpg', 'Imagenes/Imagen3.jpg', 'Imagenes/Imagen1.webp'];
 let currentIndex = 0;
+let intervalId;
 
 const imageConteiner = document.getElementById('image-conteiner');
+const progressBar = document.getElementById('progress-bar');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 
@@ -19,11 +21,7 @@ function updateImage(direction) {
     imageConteiner.appendChild(newImage);
     
     requestAnimationFrame(() => {
-        if (direction === 'next') {
-            newImage.classList.add('image-enter-active');
-        } else {
-            newImage.classList.add('image-enter-active');
-        }
+        newImage.classList.add('image-enter-active');
     });
     
     setTimeout(() => {
@@ -32,12 +30,39 @@ function updateImage(direction) {
     }, 1000);
 }
 
+function resetInterval() {
+    clearInterval(intervalId);
+    intervalId = setInterval(autoSlide, 5000);
+    resetProgressBar();
+}
+
+function resetProgressBar() {
+    progressBar.style.transition = 'none';
+    progressBar.style.width = '0%';
+    requestAnimationFrame(() => {
+        progressBar.style.transition = 'width 5s linear';
+        progressBar.style.width = '100%';
+    });
+}
+
+function autoSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage('next');
+    resetProgressBar();
+}
+
 prevBtn.addEventListener('click', () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage('prev');
+    resetInterval();
 });
 
 nextBtn.addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % images.length;
     updateImage('next');
+    resetInterval();
 });
+
+
+intervalId = setInterval(autoSlide, 5000);
+resetProgressBar();
